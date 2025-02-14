b4e3475596b66768e22feeaa66b314a1be8bdeab
tombola <tombola@github>
2025-02-14 11:25:46 +0000

    Test DDR xml parsing

---
+++ tests/data/inventory_test_models.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+import datetime
+from typing import Any
+
+
+class Products(BaseModel):
+    date: datetime.date = Field(None)
+    category: str = Field(None)
+    name: str = Field(None)
+    manufacturer: str = Field(None)
+    model_year: float = Field(None)
+    part_number: str = Field(None)
+    bar_code: str = Field(None)
+    description: str = Field(None)
+    image: str = Field(None)
+    location: str = Field(None)
+    taxable: str = Field(None)
+    unit_cost: float = Field(None)
+    unit_price: float = Field(None)
+    units_on_hand: float = Field(None)
+    last_order: datetime.date = Field(None)
+    stock_value: float = Field(None)
+    primarykey: str = Field(None, description="Unique identifier of each record in this table")
+    createdby: str = Field(None, description="Account name of the user who created each record")
+    modifiedby: str = Field(None, description="Account name of the user who last modified each record")
+    availability: str = Field(None)
+    reorder_level: float = Field(None)
+    creationtimestamp: str = Field(None, description="Date and time each record was created")
+    modificationtimestamp: str = Field(None, description="Date and time each record was last modified")
+
+
+class InventoryTransactions(BaseModel):
+    primarykey: str = Field(None, description="Unique identifier of each record in this table")
+    createdby: str = Field(None, description="Account name of the user who created each record")
+    modifiedby: str = Field(None, description="Account name of the user who last modified each record")
+    lot_number: str = Field(None)
+    description: str = Field(None)
+    type: str = Field(None)
+    units: float = Field(None)
+    foreignkey: str = Field(None, description="Unique identifier of each record in the related table")
+    date: datetime.date = Field(None)
+    units_in_|_out: float = Field(None)
+    creationtimestamp: str = Field(None, description="Date and time each record was created")
+    modificationtimestamp: str = Field(None, description="Date and time each record was last modified")
--- tests/test_ddr.py
+++ tests/test_ddr.py
@@ -1,9 +1,11 @@
 from generate_fm_pydantic import parse_fm_ddr
-
+import pytest
 
 def test_parse_xml():
-    tables = parse_fm_ddr("tmp/test_ddr.xml")
-    assert tables == [
-        ("Table1", [("Field1", "str"), ("Field2", "float")]),
-        ("Table2", [("Field1", "str"), ("Field2", "date")]),
-    ]
+    tables = parse_fm_ddr("tests/data/inventory_test_fmp12.xml")
+    assert list(tables.keys()) == ["Products", "Inventory Transactions"]
+    assert tables["Products"]["Date"]["field_type"] == "datetime.date"
+    assert tables["Products"]["CreationTimestamp"]["field_type"] == "str"
+    assert tables["Products"]["CreationTimestamp"]["field_comment"] == "Date and time each record was created"
+    print(tables)
+
